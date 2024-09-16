b3e7c8bc2fac2286a76cec8e70e5f82c152ce1d3
John Bonilla <jbonilla@neworg.com>
2024-09-16 19:29:23 -0400

    visual elements done

---
+++ src/Board.js
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+import './Board.css';
+import Card from './Card';
+
+const Board = () => {
+    const sampleCards = [
+        { id: 1, title: '9 of Clubs', imageUrl: 'https://deckofcardsapi.com/static/img/9C.png' },
+        { id: 2, title: '7 of Spades', imageUrl: 'https://deckofcardsapi.com/static/img/7S.png' },
+        { id: 3, title: '2 of Diamonds', imageUrl: 'https://deckofcardsapi.com/static/img/2D.png' },
+    ];
+
+    return (
+        <div className="board-container">
+            <div className="buttons-container">
+                <button>Give me a card</button>
+                <button>Reshuffle</button>
+            </div>
+            <div className="board">
+                {sampleCards.map((card) => (
+                    <Card key={card.id} title={card.title} imageUrl={card.imageUrl} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Board;
---
+++ src/Card.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import './Card.css';
+
+const Card = ({ imageUrl, title }) => {
+    return (
+        <div className="card">
+            <img src={imageUrl} alt={title} />
+        </div>
+    );
+};
+
+export default Card;
